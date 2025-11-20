4d09e87fbbe94143ccf77d26e15da292ea995e88
kimgho <kgh67811@knu.ac.kr>
2025-11-20 15:54:24 +0900

    fix(App): button props 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Button } from './stories/Button';
+import { Button } from './components/button/Button';
 
 function App() {
   return (
-    <Button size="sm" visual="outline">
+    <Button size="sm" variant="outline">
       asd
     </Button>
   );
