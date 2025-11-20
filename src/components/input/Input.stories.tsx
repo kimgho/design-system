6b3dfd80556291284f4bacc0cc39a5c3de56835b
kimgho <kgh67811@knu.ac.kr>
2025-11-20 23:49:42 +0900

    fix(Input): input 폴더를 소문자로 변경

--- src/components/input/Input.stories.tsx
+++ src/components/input/Input.stories.tsx
@@ -1,4 +1,4 @@
-import { Input } from '@/components/Input/Input';
+import { Input } from '@/components/input/Input';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { css } from '@styled-system/css';
 
