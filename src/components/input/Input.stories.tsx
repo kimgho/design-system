34f1903378e554c39aa3c3220d2a21c2b11effb2
kimgho <kgh67811@knu.ac.kr>
2025-11-22 16:51:30 +0900

    refactor(Input): input 문서 변경

--- src/components/input/Input.stories.tsx
+++ src/components/input/Input.stories.tsx
@@ -5,7 +5,6 @@ import { css } from '@styled-system/css';
 const meta: Meta<typeof Input> = {
   title: 'Component/Input',
   component: Input,
-  tags: ['autodocs'],
   decorators: [
     Story => (
       <div className={css({ m: 10, maxW: '500px' })}>
@@ -24,16 +23,25 @@ export const Default: Story = {
     description: '설명이 들어갑니다.',
   },
 };
-export const Required: Story = {
-  args: {
-    label: '필수 필드입니다',
-    required: true,
-    description: '필수 필드입니다.',
-  },
+
+export const Status: Story = {
+  render: () => (
+    <div className={css({ display: 'flex', flexDirection: 'column', gap: '6' })}>
+      <Input size="sm" label="Default(No Status)" placeholder="Default(No Status)" description="기본" />
+      <Input size="sm" label="Required" placeholder="required" required={true} description="필수" />
+      <Input size="sm" label="Invalid" placeholder="invalid" errorText="에러 메시지입니다" />
+      <Input size="sm" label="Disable" placeholder="disable" disabled={true} description="비활성" />
+    </div>
+  ),
 };
-export const Invalid: Story = {
-  args: {
-    label: '에러',
-    errorText: '에러입니다',
-  },
+
+export const Sizes: Story = {
+  args: {},
+  render: () => (
+    <div className={css({ display: 'flex', flexDirection: 'column', gap: '6' })}>
+      <Input size="sm" label="Small" placeholder="Small input" />
+      <Input size="md" label="Medium" placeholder="Medium input" />
+      <Input size="lg" label="Large" placeholder="Large input" />
+    </div>
+  ),
 };
